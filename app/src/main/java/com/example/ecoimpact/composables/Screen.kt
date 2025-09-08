a7174db85b5181bfc33d22e36d698a035557146f
RaphaelSouzaFranco <raphchio@gmail.com>
2025-09-07 21:17:46 -0300

    UPDATE: Screens

--- app/src/main/java/com/example/ecoimpact/composables/Screen.kt
+++ app/src/main/java/com/example/ecoimpact/composables/Screen.kt
@@ -4,4 +4,5 @@ sealed class Screen (val route: String){
     data object Login: Screen ("login")
     data object Dashboard: Screen ("dashboard")
     data object Carbon: Screen ("carbon")
+    data object Final: Screen ("final")
 }
